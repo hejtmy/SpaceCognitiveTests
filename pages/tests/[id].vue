1523ee383ee537b220082e47723b39b4cd8e7d07
Lukáš Hejtmánek <hejtmy@gmail.com>
2024-11-29 19:07:55 +0100

    updates content test template

--- pages/tests/[id].vue
+++ pages/tests/[id].vue
@@ -39,7 +39,7 @@ const { data } = await useAsyncData('tests', () => queryContent(`tests/${route.p
                         </figure>
                         <div class="prose max-w-none text-slate-400 prose-headings:text-slate-50 prose-h2:text-xl prose-h2:mt-8 prose-h2:mb-4 prose-p:leading-relaxed prose-a:text-purple-500 prose-a:no-underline hover:prose-a:underline prose-strong:text-slate-50 prose-strong:font-medium prose-blockquote:pl-5 prose-blockquote:xl:-ml-5 prose-blockquote:border-l-2 prose-blockquote:border-purple-500 prose-blockquote:font-medium prose-blockquote:text-slate-300 prose-blockquote:italic"></div>
                         <article>
-                          <ContentDoc />
+                          <ContentDoc class="text-slate-400 space-y-6" />
                         </article>
                       </article>
                       <Quote />
@@ -62,8 +62,6 @@ const { data } = await useAsyncData('tests', () => queryContent(`tests/${route.p
         </div>
       </section>
     </main>
-    <!-- Site footer -->
-    <Footer />
-    
+    <CestaFooter />
   </div>
 </template>
